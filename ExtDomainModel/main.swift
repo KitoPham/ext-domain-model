ebbb122bc36567f57f915ce94896d7558ca0713d
kito pham <kpham97@live.com>
2017-04-12 15:04:14 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by Kito T. Pham on 4/12/17.
+//  Copyright © 2017 Kito T. Pham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
